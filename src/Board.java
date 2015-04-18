302e9840493d3cf8c68c992727d5fabd193e9d0b
BaconRemovalUnit <suizsq@qq.com>
2015-04-18 12:20:23 -0400

    more updates
    Basic methods for Board class

--- src/Board.java
+++ src/Board.java
@@ -12,11 +12,45 @@ public class Board {
 		board[4][4]=-1;
 		board[3][4]=1;
 		board[4][3]=1;
+		place(5,5,true);
 	}
 	
+	public void place(int x, int y, boolean side){
+		if(canPlace(x,y))
+		try{
+			if(side)
+			board[x][y] = 1;
+			else
+			board[x][y] = -1;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean canPlace(int x, int y) {
+		try{
+			return (board[x][y]==0);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean boardFilled(){
+		for(int j = 0; j<board.length; j++){
+			for(int i = 0; i<board[0].length;i++){
+				if(board[i][j] ==0)
+					return false;
+			}
+		}
+		return true;
+	}
+
 	public void print(){
-		for(int i = 0; i<board.length; i++){
-			for(int j = 0; j<board[0].length;j++){
+		for(int j = 0; j<board.length; j++){
+			for(int i = 0; i<board[0].length;i++){
 				if(board[i][j] == 1)
 					System.out.print("O\t");
 				else if(board[i][j] == -1)
---
+++ src/MinmaxTree.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class MinmaxTree {
+	ArrayList<MinmaxTree> child = new ArrayList<MinmaxTree>();
+	MinmaxTree Parent = null;
+	int depth;
+	
+	public MinmaxTree(){
+		this.depth = 0;
+	}
+	
+	
+
+}
